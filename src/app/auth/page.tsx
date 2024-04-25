53dbe7a1714f7e8a15e5f6c342784d1bc5eaabed
Martin R. O <me@snake.rip>
2024-04-25 08:23:55 -0300

    Update auth page to include Google sign-in button

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,23 @@
+import GoogleButton from '@/components/auth/google_button';
 import React from 'react';
 
 const page = () => {
-    return <div>Hello world</div>;
+    return (
+        <div className='flex flex-1 items-center'>
+            <div className=' flex items-end gap-8 rounded-xl bg-white/10 p-8'>
+                <div>
+                    <h1 className='text-3xl font-semibold text-white'>
+                        Sign in
+                    </h1>
+                    <p className='text-gray-400'>
+                        To continue to the app, sign in with Google
+                    </p>
+                </div>
+
+                <GoogleButton />
+            </div>
+        </div>
+    );
 };
 
 export default page;
