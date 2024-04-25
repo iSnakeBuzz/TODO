cd649736f238ba31a680dfbc1ae345893c0305bf
Martin R. O <me@snake.rip>
2024-04-25 10:26:44 -0300

    Add AuthProvider component for managing user authentication

---
+++ src/providers/auth.provider.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import Cookies from 'js-cookie';
+import { User } from 'firebase/auth';
+import { auth } from '@/firebase/client.config';
+
+type AuthContextProps = {
+    user: User | null;
+};
+
+export const AuthContext = React.createContext<AuthContextProps>({
+    user: null,
+});
+
+// Update the cookie stid (Session Token ID)
+function updateCookie(user: User | null) {
+    if (user) {
+        user.getIdToken().then((token) => {
+            Cookies.set('stid', token, { expires: 7, path: '/' });
+        });
+    } else {
+        Cookies.remove('stid', { path: '/' });
+    }
+}
+
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+    const [user, setUser] = React.useState<User | null>(null);
+
+    React.useEffect(() => {
+        const unsubAuthStateChanged = auth.onAuthStateChanged((user) => {
+            updateCookie(user);
+            setUser(user);
+        });
+        const unSubIdTokenChanged = auth.onIdTokenChanged((user) => {
+            updateCookie(user);
+            setUser(user);
+        });
+
+        return () => {
+            unsubAuthStateChanged();
+            unSubIdTokenChanged();
+        };
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
