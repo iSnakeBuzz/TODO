cf5d65f735c90cd897b5e46cabc262068eb601ff
Martin R. O <me@snake.rip>
2024-04-26 10:52:22 -0300

    Add TodoInput and SessionAvatar components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import TodoInput from '@/components/home/todo-input';
+import SessionAvatar from '@/components/session/session-avatar';
+
 export default function Home() {
     return (
-        <div>
+        <div className='w-full max-w-3xl'>
+            <div className='my-6 flex items-center gap-4'>
+                <TodoInput />
+                <SessionAvatar />
+            </div>
+
             <h1>Hello, world!</h1>
             <p>
                 This is a simple Next.js app with Firebase authentication. You
