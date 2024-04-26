a923001fbb174a7316097203ce2595cd6109685e
Martin R. O <me@snake.rip>
2024-04-26 10:52:06 -0300

    Add SessionAvatar component for displaying user avatar in session

---
+++ src/components/session/session-avatar.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import useAuth from '@/hooks/useAuth';
+
+const SessionAvatar = () => {
+    const { user } = useAuth();
+
+    return (
+        <div className='flex items-center rounded-full p-0.5 ring-2 ring-orange-500/50'>
+            <Avatar>
+                <AvatarImage src={user?.photoURL ?? ''} />
+                <AvatarFallback className='bg-blue-900 text-xs font-semibold'>
+                    {user?.displayName
+                        ?.split(' ')
+                        .map((name) => name[0])
+                        .join('')}
+                </AvatarFallback>
+            </Avatar>
+        </div>
+    );
+};
+
+export default SessionAvatar;
