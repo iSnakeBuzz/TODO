b68e3f4d9df7b651058b87109ac3d0090afc2fb1
Martin R. O <me@snake.rip>
2024-04-26 14:30:04 -0300

    Update useAuth import in session-avatar.tsx

--- src/components/session/session-avatar.tsx
+++ src/components/session/session-avatar.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
-import useAuth from '@/hooks/useAuth';
+import useAuth from '@/hooks/use-auth';
 
 const SessionAvatar = () => {
     const { user } = useAuth();
 
     return (
-        <div className='flex items-center rounded-full p-0.5 ring-2 ring-orange-500/50'>
+        <div className='flex items-center rounded-full bg-black p-0.5 ring-2 ring-orange-500/50'>
             <Avatar>
                 <AvatarImage src={user?.photoURL ?? ''} />
                 <AvatarFallback className='bg-blue-900 text-xs font-semibold'>
